92a7d52cf563db066e6b117c5430927df12cdc53
ryucb <62998734+ryucb@users.noreply.github.com>
2023-06-15 18:44:53 +0900

    stack

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception{
+        
 	}
 
 }
